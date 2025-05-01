555782dca25fa5bbbaef41174b1a7e0acb75a6fc
rasegqw <yunsuk224@gmail.com>
2025-05-01 10:08:30 +0900

    lastone

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -11,7 +11,7 @@
 
 /* 팀 정보 */
 team_t team = {
-    "ateam", "Harry Bovik", "bovik@cs.cmu.edu", "", ""
+    "8조", "홍길동", "jungle_campus", "", ""
 };
 
 /* 매크로 */
@@ -105,35 +105,33 @@ static void add_free_block(void *bp)
 {
     // cur 은 free list의 맨 앞.
     void *cur = heap_listp;
-    void *prev = NULL;
+    // // version1(LIFO) : 맨앞에 추가하기 때문에, 추가 시간복잡도 O(1)
+    // heap_listp = bp;
 
-    // version 1 : 맞는 위치 찾아가야 해서, 시간복잡도 O(N)
-    // free 블럭의 주소 순서대로 free list의 순서를 설정한 것 같음.
-    // while (cur != NULL && cur < bp) {
-    //     prev = cur;
-    //     cur = GET_SUCC(cur);
-    // }
-
-    // if (prev != NULL)
-    //     GET_SUCC(prev) = bp;
-    // else
-    //     heap_listp = bp;
+    // GET_PRED(heap_listp) = NULL;
+    // GET_SUCC(heap_listp) = cur;
 
     // if (cur != NULL)
-    //     GET_PRED(cur) = bp;
+    //     GET_PRED(cur) = heap_listp;
 
-    // GET_PRED(bp) = prev;
-    // GET_SUCC(bp) = cur;
+    // version2(주소 지정 방식) : 맞는 위치 찾아가야 해서, 시간복잡도 O(N)
+    void *prev = NULL;
+    // free 블럭의 주소 순서대로 free list의 순서를 설정한 것 같음.
+    while (cur != NULL && cur < bp) {
+        prev = cur;
+        cur = GET_SUCC(cur);
+    }
 
-    // 근데 나는 그냥 새로 추가할 free 블록들은 모두 free list 맨 앞에 넣고 싶음.
-    // version 2 : 맨앞에 추가하기 때문에, 추가 시간복잡도 O(1)
-    heap_listp = bp;
-
-    GET_PRED(heap_listp) = NULL;
-    GET_SUCC(heap_listp) = cur;
+    if (prev != NULL)
+        GET_SUCC(prev) = bp;
+    else
+        heap_listp = bp;
 
     if (cur != NULL)
-        GET_PRED(cur) = heap_listp;
+        GET_PRED(cur) = bp;
+
+    GET_PRED(bp) = prev;
+    GET_SUCC(bp) = cur;
 
 }
 // 주소 지정 방식 VS LIFO <- 둘의 장단점 및 비교
@@ -241,55 +239,6 @@ static void *coalesce(void *bp)
 
 /////////////////////////////////////////////////////////////////////
 /* find_fit - Best Fit */
-static void *find_fit(size_t asize)
-{
-    // asize보다 큰 곳 찾기. 근데 가장 작은 놈으로.
-    void *bp = heap_listp;
-    void *best = NULL;
-    // 이건 -1로 설정하는게 아님. 최대 사이즈로 설정한거임.
-    size_t best_size = (size_t)(-1);
-
-    // free list 처음부터 쭉 찾기 시작합니다
-    while (bp != NULL) {
-        // free node 의 크기를 파악하고,
-        size_t bsize = GET_SIZE(HDRP(bp));
-        // 만약에 free node의 크기가 더 크다?
-        if (bsize >= asize) {
-            // 근데 free node 의 크기가 현재 best node의 크기보다 작다?
-            // 그럼 best node 갈아껴줘야지.
-            if (bsize < best_size) {
-                best = bp;
-                best_size = bsize;
-            }
-        }
-        // 하고 bp 다음으로 바꿔주고.
-        bp = GET_SUCC(bp);
-    }
-
-    // 다 돌면, best로 설정이 돼있겠지
-    return best;
-}
-
-/////////////////////////////////////////////////////////////////////
-// 이건 first-fit 할거.
-// static void *find_fit(size_t asize)
-// {
-//     // free list 시작부터
-//     void *bp = heap_listp;
-    
-//     while (bp != NULL) {
-//         size_t bsize = GET_SIZE(HDRP(bp));
-//         if (bsize >= asize) {
-//             return bp;
-//         }
-//         bp = GET_SUCC(bp);
-//     }
-    
-//     return NULL;
-// }
-
-/////////////////////////////////////////////////////////////////////
-// // 이건 Next fit
 // static void *find_fit(size_t asize)
 // {
 //     // asize보다 큰 곳 찾기. 근데 가장 작은 놈으로.
@@ -319,7 +268,23 @@ static void *find_fit(size_t asize)
 //     return best;
 // }
 
-//////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+// 이건 first-fit 할거.
+static void *find_fit(size_t asize)
+{
+    // free list 시작부터
+    void *bp = heap_listp;
+    
+    while (bp != NULL) {
+        size_t bsize = GET_SIZE(HDRP(bp));
+        if (bsize >= asize) {
+            return bp;
+        }
+        bp = GET_SUCC(bp);
+    }
+    
+    return NULL;
+}
 
 
 
@@ -467,7 +432,6 @@ void *mm_realloc(void *ptr, size_t size)
     
     return newptr;
 }
-
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -831,5 +795,399 @@ void *mm_realloc(void *ptr, size_t size)
 //     }
 //     memcpy(newptr, ptr, copySize);
 //     mm_free(ptr);
+//     return newptr;
+// }
+
+
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+/*
+ * mm-final.c - 명시적 가용 리스트 + 주소순서 삽입 + Best Fit + realloc 최적화
+ */
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <assert.h>
+// #include <string.h>
+// #include "mm.h"
+// #include "memlib.h"
+
+// /* 팀 정보 */
+// team_t team = {
+//     "ateam", "Harry Bovik", "bovik@cs.cmu.edu", "", ""
+// };
+
+// /* 매크로 */
+// #define WSIZE 8
+// #define DSIZE 16
+// // #define WSIZE 4     // implicit
+// // #define DSIZE 8     // implicit
+// #define CHUNKSIZE (1<<12)
+
+// #define MAX(x, y) ((x) > (y) ? (x) : (y))
+// #define PACK(size, alloc) ((size) | (alloc))
+// #define GET(p) (*(unsigned int *)(p))
+// #define PUT(p, val) (*(unsigned int *)(p) = (val))
+// #define GET_SIZE(p) (GET(p) & ~0x7)
+// #define GET_ALLOC(p) (GET(p) & 0x1)
+// #define HDRP(bp) ((char *)(bp) - WSIZE)
+// #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+// #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
+// #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
+
+// #define GET_SUCC(bp) (*(void **)((char *)(bp) + WSIZE))
+// #define GET_PRED(bp) (*(void **)(bp))
+
+// /* 전역변수 */
+// static char *heap_listp;
+// static char *next_listp;    // next fit을 위한 전역변수
+
+// /* 함수 선언 */
+// static void *extend_heap(size_t words);
+// static void *coalesce(void *bp);
+// static void *find_fit(size_t asize);
+// static void place(void *bp, size_t asize);
+// static void add_free_block(void *bp);
+// static void splice_free_block(void *bp);
+
+// /* mm_init */
+// int mm_init(void)
+// {
+//     if ((heap_listp = mem_sbrk(8 * WSIZE)) == (void *)-1)
+//         return -1;
+//     PUT(heap_listp, 0);     // 맨 앞 패딩. 정렬을 위한 패딩.
+//     PUT(heap_listp + (1 * WSIZE), PACK(2 * WSIZE, 1));  // 프롤로그 헤더.
+//     PUT(heap_listp + (2 * WSIZE), PACK(2 * WSIZE, 1));  // 프롤로그 풋터.
+//     PUT(heap_listp + (3 * WSIZE), PACK(4 * WSIZE, 0));  // dummy free node 헤더.
+//     PUT(heap_listp + (4 * WSIZE), NULL);                // dummy free node, prev.
+//     PUT(heap_listp + (5 * WSIZE), NULL);                // dummy free node, next
+//     PUT(heap_listp + (6 * WSIZE), PACK(4 * WSIZE, 0));  // dummy free node 풋터.
+//     PUT(heap_listp + (7 * WSIZE), PACK(0, 1));          // 에필로그 헤더.
+
+//     // 그래서 처음 payload가 오는 곳은 heap_list에서 dummy free 헤더 다음인
+//     // 4 * WSIZE 뒤로 설정.
+//     heap_listp += (4 * WSIZE);
+//     next_listp = heap_listp;
+
+
+//     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+//         return -1;
+//     return 0;
+// }
+
+// /* extend_heap */
+// static void *extend_heap(size_t words)
+// {
+
+//     char *bp;
+
+//     size_t size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+
+//     // 이제 진짜 사이즈 늘려주고, 원래 있던 brk 값 bp에 저장.
+//     if ((long)(bp = mem_sbrk(size)) == -1)
+//         return NULL;
+    
+//     PUT(HDRP(bp), PACK(size, 0));
+//     PUT(FTRP(bp), PACK(size, 0));
+
+//     // 그리고 에필로그 다시 설정.
+//     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+//     // for next-fit
+//     next_listp = coalesce(bp);
+//     return next_listp;
+// }
+
+// /* add_free_block - 주소순서 삽입 */
+// static void add_free_block(void *bp)
+// {
+//     // cur 은 free list의 맨 앞.
+//     void *cur = heap_listp;
+//     void *prev = NULL;
+
+//     // version 1 : 맞는 위치 찾아가야 해서, 시간복잡도 O(N)
+//     // free 블럭의 주소 순서대로 free list의 순서를 설정한 것 같음.
+//     while (cur != NULL && cur < bp) {
+//         prev = cur;
+//         cur = GET_SUCC(cur);
+//     }
+
+//     if (prev != NULL) {
+//         GET_SUCC(prev) = bp;
+//     }
+//     else {
+//         heap_listp = bp;
+//     }
+
+//     if (cur != NULL) {
+//         GET_PRED(cur) = bp;
+//     }
+    
+//     GET_PRED(bp) = prev;
+//     GET_SUCC(bp) = cur;
+
+// }
+
+// /* splice_free_block */
+// static void splice_free_block(void *bp)
+// {
+//     // 받아온 주소의 블록을 free list에서 제거해야 하는상황
+//     // 설명을 간단히 하기 위해 선임자를 1, 자신을 2, 후임자를 3으로 하겠음.
+//     // 1 - 2 - 3 이렇게 연결되어 있는 형태에서 2를 제거하고 싶은거임.
+
+//     // 만약 2의 선임자가 있다?
+//     if (GET_PRED(bp))
+//         // 있으면 선임자의 후임자를 받아온 블록의 후임자로 이어주기.
+//         // 1 -> 3 이렇게 된거임.
+//         GET_SUCC(GET_PRED(bp)) = GET_SUCC(bp);
+    
+//     // 만약 2의 선임자가 없다? -> 이말은 2가 리스트의 맨 앞이라는 거임.
+//     else
+//         // 그러면 그냥 2의 후임자가 free list의 맨 앞이 되면 끝.
+//         heap_listp = GET_SUCC(bp);
+
+//     // 만약 2의 후임자가 있다?
+//     if (GET_SUCC(bp))
+//         // 그러면 2의 후임자의 전임자를, 2의 전임자로 이어주기
+//         // 1 <- 3 이렇게 만들어준거임.
+//         GET_PRED(GET_SUCC(bp)) = GET_PRED(bp);
+// }
+
+// /* coalesce */
+// static void *coalesce(void *bp)
+// {
+//     // 받아온 노드의 주소 이전, 다음 블록의 주소 저장.
+//     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+//     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+//     // 현재 블록의 크기도 저장.
+//     size_t size = GET_SIZE(HDRP(bp));
+
+//     // 만약 이전, 다음 블록 모두 할당이 되어있다?
+//     if (prev_alloc && next_alloc) {
+//         // 그러면 그냥 지금 상태 그대로 free list에 넣어주면 됨.
+//         add_free_block(bp);
+
+//         // 추가했으면 끝.
+//         return bp;
+//     }
+//     // 근데 이전은 할당이 되어있고, 다음은 free 상태다?
+//     else if (prev_alloc && !next_alloc) {
+//         // 그러면 일단 다음 블록이랑 합쳐줘야 하니까,
+//         // 다음 블록을 free list에서 빼줘야 함.
+//         splice_free_block(NEXT_BLKP(bp));
+
+//         // 다음 free 블록의 크기까지 더해줘서
+//         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+//         // 현재 헤더에 변경된 size와 할당 여부를 바꿔주고,
+//         PUT(HDRP(bp), PACK(size, 0));
+//         // size가 바뀌엇기 때문에, 풋터의 위치도 알아서 바뀌어있음.
+//         PUT(FTRP(bp), PACK(size, 0));
+//     }
+//     // 이전은 free 이고, 다음은 할당이 되어있다?
+//     else if (!prev_alloc && next_alloc) {
+//         splice_free_block(PREV_BLKP(bp));
+
+//         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+
+
+//         bp = PREV_BLKP(bp);
+//         PUT(HDRP(bp), PACK(size, 0));
+//         PUT(FTRP(bp), PACK(size, 0));
+//         // ??? : 이렇게 하면 안되나요?
+//         // -> ㅇㅇ 됨. 사실 이게 더 안전함.
+//     }
+//     // 이제 둘다 free 인 경우
+//     else {
+//         // 이전도, 다음도 모두 free list에서 제거 해줘야 함.
+//         splice_free_block(PREV_BLKP(bp));
+//         splice_free_block(NEXT_BLKP(bp));
+//         // 사이즈도 다시 재조정 해주고,
+//         size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+//         // 주소를 이전 블록의 payload로 다시 맞춰주고
+//         bp = PREV_BLKP(bp);
+//         // 헤더, 푸터 다시 설정.
+//         PUT(HDRP(bp), PACK(size, 0));
+//         PUT(FTRP(bp), PACK(size, 0));
+//     }
+
+//     add_free_block(bp);
+
+//     return bp;
+// }
+
+// static void *find_fit(size_t asize)
+// {
+//     // asize보다 큰 곳 찾기. 근데 가장 작은 놈으로.
+//     void *bp = next_listp;
+//     void *home = heap_listp;
+
+//     // free list 처음부터 쭉 찾기 시작합니다
+//     for(;bp != NULL;bp = GET_SUCC(bp))
+//     {
+//         size_t bsize = GET_SIZE(HDRP(bp));
+//         if (bsize >= asize)
+//         {
+//             return bp;
+//         }
+//     }
+//     for(bp = home; bp != next_listp ; bp = GET_SUCC(bp))
+//     {
+//         size_t bsize = GET_SIZE(HDRP(bp));
+//         if (bsize >= asize)
+//         {
+//             return bp;
+//         }
+//     }
+    
+//     return NULL;
+// }
+
+
+// /* place */
+// static void place(void *bp, size_t asize)
+// {
+//     // bp 가 들어갈 free 블럭의 사이즈를 일단 받아오고
+//     size_t csize = GET_SIZE(HDRP(bp));
+    
+//     ///// - for next-fit - /////
+
+//     next_listp = GET_SUCC(bp);
+//     if (next_listp == NULL)
+//         next_listp = heap_listp;
+
+//     ////////////////////////////
+
+//     // 해당 free 블록을 free list에서 없애주기
+//     splice_free_block(bp);
+
+
+//     // 만약에 free 블록을 사용할때, 쓰고 남은 크기가 32바이트보다 크면,
+//     if ((csize - asize) >= (2 * DSIZE)) {
+//         // 해당 블록에 사용할 크기만 할당해주고
+//         PUT(HDRP(bp), PACK(asize, 1));
+//         PUT(FTRP(bp), PACK(asize, 1));
+
+//         // 남은 free 블록은 바로 다음 블록을 테니까
+//         bp = NEXT_BLKP(bp);
+//         // 남은 free 블록은 사이즈 재정의
+//         PUT(HDRP(bp), PACK(csize - asize, 0));
+//         PUT(FTRP(bp), PACK(csize - asize, 0));
+//         // 다 했으면 남은 free 블록은 free list에 추가해주기.
+//         add_free_block(bp);
+
+//         // for next-fit
+//         next_listp = bp;
+//     }
+//     // 만약 남은 크기가 32바이트보다 작으면 그냥 다 쓰지 뭐. 
+//     else
+//     {
+//         PUT(HDRP(bp), PACK(csize, 1));
+//         PUT(FTRP(bp), PACK(csize, 1));
+//     }
+
+// }
+
+// /* mm_malloc */
+// void *mm_malloc(size_t size)
+// {
+//     // size만큼 할당할 공간 만들어줘야 함.
+//     size_t asize;
+//     size_t extendsize;
+//     char *bp;
+//     // 만약 0을 늘려라? 장난치나; 바로 NULL
+//     if (size == 0)
+//         return NULL;
+
+//     // 늘려야 할 크기가 정렬 기준인 16바이트보다 작다?
+//     if (size <= DSIZE)
+//         asize = 2 * DSIZE;
+//     // 크기가 16보다 크면?
+//     else
+//         // 이렇게 크기에 따라서 패딩해주면 됨.
+//         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+//     // 만약에 fit한곳을 찾지못하면 확장하고, fit한 곳이 있으면 거기 넣기.
+//     if ((bp = find_fit(asize)) != NULL) {
+//         // 있으니까 넣어줘야겠지
+//         place(bp, asize);
+//         return bp;
+//     }
+
+//     extendsize = MAX(asize, CHUNKSIZE);
+//     // 해서 확장을 할건데 확장에 실패하면 NULL 반환하고,
+//     if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+//         return NULL;
+//     // 확장이 됐으면, 확장이 된 공간에 정보를 넣으면 되겠지?
+//     place(bp, asize);
+//     return bp;
+// }
+
+// /* mm_free */
+// void mm_free(void *bp)
+// {
+//     // free로 만들 블록의 크기를 일단 받고
+//     size_t size = GET_SIZE(HDRP(bp));
+
+//     // 그 크기만큼 다 메모리 할당 여부 0으로 해주면 됨.
+//     PUT(HDRP(bp), PACK(size, 0));
+//     PUT(FTRP(bp), PACK(size, 0));
+//     // 이후에 인접 블록들과 병합
+//     next_listp = coalesce(bp);
+// }
+
+// /* mm_realloc - in-place 최적화 */
+// // 이미 할당된 블록의 크기를 변경해주는게 realloc
+// // 변경된 블록의 payload 시작주소 반환하면 되겠지.
+// void *mm_realloc(void *ptr, size_t size)
+// {
+//     // ptr은 현재 블록의 주소
+//     // 만약에 블록이 NULL이면, size크기만큼 새로 할당해줘야겠지.
+//     if (ptr == NULL)
+//         return mm_malloc(size);
+//     // 만약에 변경하고 싶은 블록의 크기가 0이면, 메모리를 해제하겠다는 말과 같으니까
+//     // 바로 free
+//     if (size == 0) {
+//         mm_free(ptr);
+//         return NULL;
+//     }
+
+//     // 현재 블록의 크기 일단 저장해두고
+//     size_t oldsize = GET_SIZE(HDRP(ptr));
+//     // 정렬 기준을 맞추기 위해 패딩까지 합칠 블록의 크기 설정 -> asize
+//     size_t asize = (size <= DSIZE) ? (2 * DSIZE) : DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+//     // 만약 asize가 기존 사이즈보다 작다? 즉, 블록을 축소하고 싶다?
+//     if (asize <= oldsize)
+//         // 어쩌라고 안줄일거야. 왜냐면 줄이게되면, 데이타 손실 생김.
+//         return ptr;
+
+//     void *next = NEXT_BLKP(ptr);
+//     // 다음 블록이 free이고, 크기가 충분하면,
+//     if (!GET_ALLOC(HDRP(next)) && (oldsize + GET_SIZE(HDRP(next))) >= asize) {
+//         // free list에서 블록 빼주고
+//         splice_free_block(next);
+//         // 다시 크기 재정의 해서 헤더, 풋터에 수정
+//         size_t newsize = oldsize + GET_SIZE(HDRP(next));
+//         PUT(HDRP(ptr), PACK(newsize, 1));
+//         PUT(FTRP(ptr), PACK(newsize, 1));
+//         return ptr;
+//     }
+
+//     void *newptr = mm_malloc(size);
+//     // 만약에 더이상 할당할 수 없으면 바로 리턴
+//     if (newptr == NULL)
+//         return NULL;
+
+//     size_t copySize = oldsize - DSIZE;
+//     // 사이즈가 작아지길 원하면, payload의 부분을 줄여서 ㄱㄱ
+//     if (size < copySize)
+//         copySize = size;
+
+//     memcpy(newptr, ptr, copySize);
+//     mm_free(ptr);
+
 //     return newptr;
 // }
